817a86ea305ff6326a6d573a53ad1e8847ac58d3
P-M-Manmohan <manu6520042@gmail.com>
2026-05-16 19:02:24 +0900

    changed download resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,8 +28,8 @@ export const AboutSection = () => {
                             Get in Touch
                         </a>
 
-                        <a href="../../public/Resume Manmohan P M-1.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
-                            Download Resume
+                        <a href="../../public/Resume Manmohan P M-1.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            Download Resume 
                         </a>
 
                     </div>
